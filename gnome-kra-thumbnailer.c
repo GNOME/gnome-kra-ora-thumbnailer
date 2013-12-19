885a68a26e38c4f1adbe36fd1d6da2da6d88e638
Bastien Nocera <hadess@hadess.net>
2013-12-19 15:46:34 +0100

    COPYING: Another license change
    This time, to GPLv2+, as the WTFPL isn't compatible with the GPLv2+
    used in the skeleton files.

--- gnome-kra-thumbnailer.c
+++ gnome-kra-thumbnailer.c
@@ -3,10 +3,19 @@
  *
  * Authors: Bastien Nocera <hadess@hadess.net>
  *
- *          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
 
